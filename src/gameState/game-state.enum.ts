ac637efc5df8234a26e4f36615859fd25dba3681
Jens Ungerer <github@jens-ungerer.de>
2019-06-02 01:44:13 +0200

    renaming folder. plus introducing gameState-enum

---
+++ src/gameState/game-state.enum.ts
@@ -0,0 +1,8 @@
+export enum GameState {
+    NotTurn = 'NOT_TURN',
+    GameNotStarted = 'GAME_NOT_STARTED',
+    Turn = 'TURN',
+    GameLost = 'GAME_LOST',
+    GameWon = 'GAME_WON',
+    InitializationError = 'INITIALIZATION_ERROR'
+}
